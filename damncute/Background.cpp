13ec4dba22752a1d3292c45360468dba2f1d9d97
newincpp <newincpp@gmail.com>
2013-09-25 16:07:53 +0000

    add DamnCute::Background

---
+++ damncute/Background.cpp
@@ -0,0 +1,37 @@
+#include <glm/gtc/matrix_transform.hpp>
+#include "Background.hh"
+
+DamnCute::Background::Background(const std::string& texfile) : _pos(1, 1, -1) , _tex() {
+    _tex.loadFromFile(texfile);
+    _tex.setSmooth(true);
+    _tex.setRepeated(false);
+    _bg.setTexture(_tex);
+}
+
+DamnCute::Background::~Background() {
+}
+
+void DamnCute::Background::setScrollSpeed(float x, float y) {
+    _XSpeed = x;
+    _YSpeed = y;
+}
+
+float DamnCute::Background::getXScrollSpeed() const {
+    return _XSpeed;
+}
+
+float DamnCute::Background::getYScrollSpeed() const {
+    return _YSpeed;
+}
+
+#include <iostream>
+
+void DamnCute::Background::update(sf::RenderWindow* w_ptr) {
+    //_pos.x += _XSpeed / 10000;
+    //_pos.y += _YSpeed / 10000;
+    //std::cout << "speed = " << _XSpeed << " " << _YSpeed << std::endl;
+    //std::cout << "_pos = " << _pos.x << " " << _pos.y << std::endl;
+    //_bg.move(_pos.x,  _pos.y);
+    _bg.move(_XSpeed, _YSpeed);
+    w_ptr->draw(_bg);
+}
---
+++ damncute/Background.hh
@@ -0,0 +1,29 @@
+#ifndef ENVIRONMENT_H_
+# define ENVIRONMENT_H_
+
+#include <string>
+#include <glm/glm.hpp>
+#include <SFML/Graphics.hpp>
+#include "IRenderable.hh"
+#include "Core/Core.hh"
+
+namespace DamnCute {
+    class Background : public IRenderable {
+	public:
+	    Background(const std::string&);
+	    ~Background();
+	    void setScrollSpeed(float, float);
+	    float getXScrollSpeed() const ;
+	    float getYScrollSpeed() const ;
+	    virtual void update(sf::RenderWindow*);
+	private:
+	    Background(const Background&);
+	    float _XSpeed;
+	    float _YSpeed;
+	    sf::Sprite _bg;
+	    glm::vec3 _pos;
+	    sf::Texture _tex;
+    };
+}
+
+#endif /* !ENVIRONMENT_H_ */
