81dd8e8c99aeb47137acf4f4a205c859575e5a98
newincpp <newincpp@gmail.com>
2013-10-29 17:49:06 +0000

    quadtree Classes writed but not implemented

---
+++ damncute/Core/TreeNode.hh
@@ -0,0 +1,34 @@
+#ifndef NODE_H_
+# define NODE_H_
+
+template <typename DATANODE>
+struct TreeNode {
+    explicit TreeNode(unsigned short level) {
+	if (level) {
+	    up = new TreeNode<T>(level - 1);
+	    down = new TreeNode<T>(level - 1);
+	    left = new TreeNode<T>(level - 1);
+	    right = new TreeNode<T>(level - 1);
+	} else {
+	    up = NULL;
+	    down = NULL;
+	    left = NULL;
+	    right = NULL;
+	}
+    }
+
+    virtual ~TreeNode() {
+	delete up;
+	delete down;
+	delete left;
+	delete right;
+    }
+
+    DATANODE* data;
+    TreeNode<T>* up;
+    TreeNode<T>* down;
+    TreeNode<T>* right;
+    TreeNode<T>* left;
+};
+
+#endif /* !NODE_H_ */
